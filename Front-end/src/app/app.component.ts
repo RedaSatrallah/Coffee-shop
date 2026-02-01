d16821cffe1f12cdf618135e16fd232fb8b301ed
Redasatrallah <Redasatrallah@gmail.com>
2026-02-01 14:10:30 +0100

    structure landing page

--- Front-end/src/app/app.component.ts
+++ Front-end/src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from './components/hero/hero.component';
+import { WhyUsComponent } from './components/why-us/why-us.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BestSellersComponent } from './components/best-sellers/best-sellers.component';
+import { ProductSectionComponent } from './components/product-section/product-section.component';
+import { HowItWorksComponent } from './components/how-it-works/how-it-works.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeroComponent,
+    WhyUsComponent,FooterComponent,
+    NavbarComponent,BestSellersComponent,
+    ProductSectionComponent,HowItWorksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
