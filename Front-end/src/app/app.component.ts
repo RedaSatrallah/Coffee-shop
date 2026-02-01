6a82bc47cdaf2257d8d99d541a4e62921c143813
Redasatrallah <Redasatrallah@gmail.com>
2026-02-01 17:49:45 +0100

    Added Hero and navbar

--- Front-end/src/app/app.component.ts
+++ Front-end/src/app/app.component.ts
@@ -7,16 +7,18 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { BestSellersComponent } from './components/best-sellers/best-sellers.component';
 import { ProductSectionComponent } from './components/product-section/product-section.component';
 import { HowItWorksComponent } from './components/how-it-works/how-it-works.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,HeroComponent,
     WhyUsComponent,FooterComponent,
     NavbarComponent,BestSellersComponent,
-    ProductSectionComponent,HowItWorksComponent],
+    ProductSectionComponent,HowItWorksComponent, CommonModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Front-end';
+  
+  
 }
--- Front-end/tailwind.config.js
+++ Front-end/tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */ 
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default { 
 content: [ 
 "./src/**/*.{html,ts}", 
 ], 
 theme: { 
-extend: {}, 
+extend: {
+    fontFamily: {
+        "instrument-serif": ["Instrument Serif", ...defaultTheme.fontFamily.serif],
+        "instrument-sans": ["Instrument Sans", ...defaultTheme.fontFamily.sans],
+}, 
+ colors: {
+        'peach': '#E6C7B3',
+        "peach-light":"#FFF3EB"
+      },
 }, 
 plugins: [], 
+}
 }
